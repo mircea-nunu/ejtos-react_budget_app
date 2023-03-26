07fe84969370b1ed3b090d48d5fce6c4a50f6a3b
Mircea N <nunu.mircea@gmail.com>
2023-03-26 16:32:38 -0400

    drop-down 2
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

--- src/components/CurrencySelect.js
+++ src/components/CurrencySelect.js
@@ -1,22 +1,17 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 import Dropdown from './Dropdown';
 
 const CurrencySelect = () => {
     const {dispatch } = useContext(AppContext);
 
-    const [showMenu, setshowMenu] = useState(false);
-
-    useEffect(() => {
-        const handler = () => setshowMenu(false)
-        
-    })
     const options = [
         {value: "$" , label: "$ Dollar"},
         {value: "£" , label: "£ Pound"},
         {value: "₹" , label: "₹ Ruppee"},
         {value: "€" , label: "€ Euro"},
     ]
+
     const changeCurrency = (val)=>{
         console.log(val.selectedIndex + " " 
         + val[val.selectedIndex].text +
@@ -28,7 +23,8 @@ const CurrencySelect = () => {
     }
 
   return (
-        <Dropdown placeHolder="Currency" options={options}/>
+        <Dropdown placeHolder="Currency (" options={options} 
+        changeCurrency={changeCurrency}/>
     );
 
 };
