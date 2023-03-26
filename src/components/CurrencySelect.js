1b7207503fa874a8172981df92264a832a0794b3
Mircea N <nunu.mircea@gmail.com>
2023-03-26 13:28:23 -0400

    Select currency 1
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

--- src/components/CurrencySelect.js
+++ src/components/CurrencySelect.js
@@ -5,26 +5,31 @@ const CurrencySelect = () => {
   const {dispatch } = useContext(AppContext);
 
     const changeCurrency = (val)=>{
+        console.log(val.selectedIndex + " " 
+        + val[val.selectedIndex].text +
+        " " + val.value,)
             dispatch({
                 type: 'CHG_CURRENCY',
-                payload: val,
+                payload: val.value,
             })
     }
     
 
   return (
-    <div class="custom-select">
-    <label > Currency
-      <select
-        name="Currency" id="Currency" 
-        onChange={event=>changeCurrency(event.target.value)}>
-        <option value="$">$ Dollar</option>
-        <option value="£">£ Pound</option>
-        <option value="₹">₹ Ruppee</option>
-        <option value="€">€ Euro</option>
-    </select>	
+    <label
+        className="alert alert-success" 
+        htmlFor="currency-select" id="currency-label"> Currency (
+        <select
+            className="alert alert-success"
+            name="Currency" id="currency-select" 
+            onChange={event=>changeCurrency(event.target)}>
+            <option value="$">$ Dollar</option>
+            <option value="£">£ Pound</option>
+            <option value="₹">₹ Ruppee</option>
+            <option value="€">€ Euro</option>
+        </select>
+    )	
     </label>
-    </div>
     );
 
 };
