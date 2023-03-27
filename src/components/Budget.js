2ec96e141dfc3bb594bc1001832269c4d30da799
Mircea N <nunu.mircea@gmail.com>
2023-03-27 15:20:44 -0400

    budget value 2
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const {budget, dispatch, expenses, currency} = useContext(AppContext);
@@ -6,6 +6,23 @@ const Budget = () => {
     const totalExpenses = expenses.reduce((total, item) => {
         return (total += item.cost);
     }, 0);
+
+    const [focusInput, setfocusInput] = useState('initial');
+
+    const handleFocus = (focusStatus) => {
+        console.log(focusStatus);
+        setfocusInput(focusStatus);
+        if (focusInput ==="Unfocused") {
+            console.log(totalExpenses)
+            alert("The value cannot lower than the spendings");
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: totalExpenses,
+            });
+            setfocusInput("reset");
+        }
+    }
+   
 
     const handleChange = (e) => {
         const re = /^[0-9\b]+$/;
@@ -21,13 +38,6 @@ const Budget = () => {
             type: 'SET_BUDGET',
             payload: 20000,
         });
-        } else if (e.target.value < totalExpenses) {
-            console.log(totalExpenses)
-            alert("The value cannot lower than the spendings");
-            dispatch({
-                type: 'SET_BUDGET',
-                payload: totalExpenses,
-            });
         } else if (re.test(e.target.value)) {
             dispatch({
                 type: 'SET_BUDGET',
@@ -48,7 +58,9 @@ const Budget = () => {
                 id='budget'
                 value={budget}
                 style={{ marginLeft: '3px' , size: 10}}
-                onChange={handleChange}>
+                onChange={handleChange}
+                onFocus={(e) => handleFocus("focused")}
+                onBlur={(e) => handleFocus("Unfocused")}>
             </input>
             </p>
         </div>
