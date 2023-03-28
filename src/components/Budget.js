e21ae1d69269d435b5925e606581f8c7529d75a9
Mircea N <nunu.mircea@gmail.com>
2023-03-28 00:11:52 -0400

    budget edit 1
    Signed-off-by: Mircea N <nunu.mircea@gmail.com>

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
 const Budget = () => {
     const {budget, dispatch, expenses, currency} = useContext(AppContext);
@@ -7,43 +7,45 @@ const Budget = () => {
         return (total += item.cost);
     }, 0);
 
-    const [focusInput, setfocusInput] = useState('initial');
+    const handleFocus = (e, focusStatus) => {
+        if (e.target !== undefined) {
+            console.log(e.target.value);
 
-    const handleFocus = (focusStatus) => {
-        console.log(focusStatus);
-        setfocusInput(focusStatus);
-        if (focusInput ==="Unfocused") {
-            console.log(totalExpenses)
-            alert("The value cannot lower than the spendings");
-            dispatch({
-                type: 'SET_BUDGET',
-                payload: totalExpenses,
-            });
-            setfocusInput("reset");
+            if ((focusStatus ==="Unfocused") & (e.target.value < totalExpenses)) {
+                // console.log(totalExpenses)
+                alert("The value cannot be lower than the spendings");
+                dispatch({
+                    type: 'SET_BUDGET',
+                    payload: totalExpenses,
+                });
+            }
         }
+        focusStatus = "reset"
     }
    
 
-    const handleChange = (e) => {
+    const handleChange = (ev) => {
+
         const re = /^[0-9\b]+$/;
-        if (e.target.value === '') {
-                dispatch({
-                    type: 'SET_BUDGET',
-                    payload: e.target.value,
-                });
+        if (ev.target.value === '') {
+            dispatch({
+                type: 'SET_BUDGET',
+                payload: '',
+            });
         }
-        if (e.target.value >20000) {
+        if (ev.target.value >20000) {
             alert("The value cannot exceed 20000  £");
         dispatch({
             type: 'SET_BUDGET',
             payload: 20000,
         });
-        } else if (re.test(e.target.value)) {
+        } else if (re.test(ev.target.value)) {
             dispatch({
                 type: 'SET_BUDGET',
-                payload: e.target.value,
+                payload: ev.target.value,
             });
-        } 
+        }
+        handleFocus("reset")
     }
     
 
@@ -51,6 +53,7 @@ const Budget = () => {
         <div className='alert alert-secondary'>
             <p className="budget">Budget: {currency}
             <input
+                autoFocus
                 required='required'
                 type='number'
                 step='10'
@@ -59,8 +62,8 @@ const Budget = () => {
                 value={budget}
                 style={{ marginLeft: '3px' , size: 10}}
                 onChange={handleChange}
-                onFocus={(e) => handleFocus("focused")}
-                onBlur={(e) => handleFocus("Unfocused")}>
+                onFocus={(e) => handleFocus(e, "focused")}
+                onBlur={(e) => handleFocus(e,"Unfocused")}>
             </input>
             </p>
         </div>
